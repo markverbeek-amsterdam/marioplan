c08a718e8bf9c8a19148ddbcf0ba9f5c969e132d
markverbeek-amsterdam <markverbeek@mac.com>
2021-01-15 08:10:19 +0100

    rollback previous version

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -7,8 +7,8 @@ const ProjectList = ({projects}) => {
     <div className="project-list section">
       { projects && projects.map(project => {
         return (
-          <Link to={'/project/' + project.id} key={project.id}>
-            <ProjectSummary project={project} />
+          <Link to={'/project/'+ project.id} key={project.id} >
+          <ProjectSummary project={project} />
           </Link>
         )
       })}  
