adf0300c6461a9925e043069f1c4e3a2605381af
markverbeek-amsterdam <markverbeek@mac.com>
2021-01-15 12:51:08 +0100

    function test

--- functions/index.js
+++ functions/index.js
@@ -3,4 +3,6 @@ const functions = require("firebase-functions");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// ÷
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
